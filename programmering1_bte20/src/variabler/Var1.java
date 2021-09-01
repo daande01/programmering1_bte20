949bcec65b84d3c3813db23b1f1668ad8d2cf13c
daande1 <danielevertandersson>
2021-09-01 14:17:55 +0200

    erdf

--- programmering1_bte20/src/variabler/Var1.java
+++ programmering1_bte20/src/variabler/Var1.java
@@ -4,11 +4,16 @@ public class Var1 {
 
 	public static void main(String[] args) {
 
-		int i;  // deklarerat en variabel av datatypen integer
+		int summa;
+		int tal1;  // deklarerat en variabel av datatypen integer
 
-		i=5;  // tilldelar variabeln i värdet 5
+		tal1=5;  // tilldelar variabeln i värdet 5
 
-		System.out.println("svaret är "+i+" kr");
+
+		int tal2=7; // initsiering = deklarering och tilldelning
+		summa=tal1+tal2;
+
+		System.out.println(" 5+7 är "+summa+" kr");//
 
 	}
 
