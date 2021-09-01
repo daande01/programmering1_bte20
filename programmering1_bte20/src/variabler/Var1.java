63dd38c3153b6dde6a0177b24dd9df12b92744ea
daande1 <danielevertandersson>
2021-09-01 13:36:00 +0200

    variabler

---
+++ programmering1_bte20/src/variabler/Var1.java
@@ -0,0 +1,16 @@
+package variabler;
+
+public class Var1 {
+
+	public static void main(String[] args) {
+
+		int i;  // deklarerat en variabel av datatypen integer
+
+		i=5;  // tilldelar variabeln i värdet 5
+
+		System.out.println("svaret är "+i+" kr");
+
+	}
+
+
+}
