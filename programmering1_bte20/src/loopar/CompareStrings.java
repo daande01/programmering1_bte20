c48f7f9171bd2f221ded40087e1a55808a0f23d0
daande1 <danielevertandersson>
2021-11-17 10:02:45 +0100

    new file

--- programmering1_bte20/src/loopar/CompareStrings.java
+++ programmering1_bte20/src/loopar/CompareStrings.java
@@ -4,9 +4,23 @@ public class CompareStrings {
 
 	public static void main(String[] args) {
 
+		// primitiva datatyper int ,char,short,
+
+
+		String s1 = "hej då";
+		String s2 = "hej";
 
 
 
+		if (s1.equals("dfjkdfj")) {
+
+			System.out.println("true");
+
+		}else {
+
+			System.out.println("false");
+
+		}
 
 	}
 }
