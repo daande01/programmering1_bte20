a0a346058cd7cfa47c8cea9a52cf09945a563443
daande1 <danielevertandersson>
2022-01-21 11:37:15 +0100

    new

--- programmering1_bte20/src/garafik/Jframeinput.java
+++ programmering1_bte20/src/garafik/Jframeinput.java
@@ -2,7 +2,9 @@ package garafik;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -10,24 +12,25 @@ import javax.swing.JTextField;
 public class Jframeinput extends JFrame {
 
 	//private JButton b1=new JButton();
-	private JTextField field1=new JTextField("mata in data",30);
+	private JTextField field1=new JTextField("mata in ett tal",30);
+	private JTextField field2=new JTextField("mata in ett tal",30);
 	private JLabel lab= new JLabel("denna text kan du ändra");
-
+	private JButton b1 = new JButton("summera");
 
 	public Jframeinput() {
 
 		setLayout(new FlowLayout());
 		setVisible(true);
 		setSize(new Dimension(400,400));
-
+		lab.setFont(new Font("arial",20,30));
 		add(field1);
+		add(field2);
 		add(lab);
+		add(b1);
+		b1.addActionListener(e->{
 
-		field1.addActionListener(e->{
 
-			String input=field1.getText();
-			//System.out.println(input);
-			lab.setText(input);
+			lab.setText(""+(Integer.parseInt(field1.getText())+Integer.parseInt(field2.getText())));
 
 		});
 
