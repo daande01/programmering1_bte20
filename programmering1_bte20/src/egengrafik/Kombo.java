6ad19b4a3b4d42fde9fb11346f1d6e252b0c429a
daande1 <danielevertandersson>
2022-02-01 10:12:43 +0100

    new file

---
+++ programmering1_bte20/src/egengrafik/Kombo.java
@@ -0,0 +1,29 @@
+package egengrafik;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Kombo extends JComponent {
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+
+
+		JFrame f= new JFrame("sdsdsds");
+		f.setVisible(true);
+		JPanel p = new jpa
+
+
+
+
+	}
+
+
+
+}
